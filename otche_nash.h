eddf72cba76a3eb04e5b68f6f41dbb3a63753506
haloactief <89327041+haloactief@users.noreply.github.com>
2025-10-27 20:14:47 +0700

    amen

---
+++ otche_nash.h
@@ -0,0 +1,23 @@
+#ifndef OTCHE_NASH_H
+#define OTCHE_NASH_H
+
+using namespace std;
+
+#define даром void
+#define дай_боженька_судья switch
+#define в_случае case
+#define помилуй break
+#define по_веществу_нашему float
+#define аминь ;
+#define обычай default
+#define буква char
+#define възвратить return
+#define молвить cout
+#define слушание cin
+#define чъсло int
+#define святой_батюшка main
+#define больше >
+#define меньше <
+#define пока for
+
+#endif
